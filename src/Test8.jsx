6f725ee5b2e2a53de4c52fc85be03ab75f3b3507
Sanderzhanni <hannis@tlu.ee>
2019-11-25 12:49:32 +0200

    task8

--- src/Test8.jsx
+++ src/Test8.jsx
@@ -1,11 +1,68 @@
 import React from "react";
 
 class Test8 extends React.PureComponent {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            fullName:"",
+            address:"",
+            phoneNumber:"",
+            disabled: false
+
+        };
+    }
+
+    handleDisabled = () =>{
+        this.setState( {disabled: !this.state.disabled} )  ;
+    };
+
+    handleChange = (e) =>{
+        console.log("value", e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    handleSubmit = (e) =>{
+        console.log(this.state);
+        e.preventDefault();
+        fetch("/api/v1/users/", {
+            method: "POST",
+            body: JSON.stringify(this.state),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+            .then(res =>  res.json())
+            .catch(err =>{
+                console.log("err", err);
+            });
+    };
   render() {
     return (
       <div>
+
         <Task />
-        implement
+          <form className="ds-item style-2" onSubmit={this.handleSubmit}>
+              <div className={"row"}>
+                  <label htmlFor="fullName">Kliendi nimi</label>
+                  <input name="fullName" type="text" onChange={this.handleChange} value={this.state.fullName} disabled = {(this.state.disabled)? "disabled" : ""}/>
+              </div>
+              <div className={"row"}>
+                  <label htmlFor="burger">Aadress</label>
+                  <input name="address" type="text" onChange={this.handleChange} value={this.state.address} disabled = {(this.state.disabled)? "disabled" : ""}/>
+              </div>
+              <div className={"row"}>
+                  <label htmlFor="drink">Phone</label>
+                  <input name="phoneNumber" type="text" onChange={this.handleChange} value={this.state.phone} disabled = {(this.state.disabled)? "disabled" : ""}/>
+              </div>
+              <button style={{width: "100%"}}>
+                  Otsi
+              </button>
+
+          </form>
+          <button onClick = {this.handleDisabled.bind(this)}>Disable</button>
       </div>
     );
   }
